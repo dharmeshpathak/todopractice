bdedfac5c9845ee1b5dbc2dfe7a241b9b36355d1
dharmesh <dharmeshpathak1999@gmail.com>
2020-08-22 09:29:08 +0530

    Updates

---
+++ precache-manifest.4cea8909465ccfedd5a6f364274a8549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf389ce37310e16f981a9382c608a5e",
+    "url": "/todopractice/index.html"
+  },
+  {
+    "revision": "2ef5023c922be00123ba",
+    "url": "/todopractice/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cff4fa0f9ac8a4c4ff0f",
+    "url": "/todopractice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ef5023c922be00123ba",
+    "url": "/todopractice/static/js/2.c9e842b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todopractice/static/js/2.c9e842b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff4fa0f9ac8a4c4ff0f",
+    "url": "/todopractice/static/js/main.7d148232.chunk.js"
+  },
+  {
+    "revision": "c5ecd486fdde355e59b1",
+    "url": "/todopractice/static/js/runtime-main.94e52fe9.js"
+  }
+]);
